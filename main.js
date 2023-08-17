d93645e22e5f69d7829988b012759c098ebd46cf
Robin7339 <graphicssmart12@gmail.com>
2023-08-17 13:08:52 +0530

    Lenngth-converter

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function convert(){
+    const cmVal = Number(document.getElementById("input").value)
+    const inchVal = cmVal/2.54
+    const result = document.getElementById("result")
+    result.innerHTML = inchVal
+    
+
+}
